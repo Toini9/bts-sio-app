4b679f296ebaccaddc659cc782291c610d68bc0a
Tony891 <72949320+Tony891@users.noreply.github.com>
2021-03-15 22:11:11 +0100

    Doss XAMA
    Tous les dossiers du XAMARIN

---
+++ TestAPIGSB/TestAPIGSB/TestAPIGSB/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TestAPIGSB.ClassesMetier;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using TestAPIGSB.Pages;
+
+namespace TestAPIGSB
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSecteurs_Clicked(object sender, EventArgs e)
+        {
+            PageSecteur page = new PageSecteur();
+            await Navigation.PushModalAsync(new NavigationPage(page));
+        }
+
+        private void btnRegions_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVisiteurs_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTravailler_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
